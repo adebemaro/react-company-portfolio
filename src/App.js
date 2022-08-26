fe50229488050455d114ea143f379ad20b733679
adebemaro <adebemario@gmail.com>
2022-08-25 18:38:09 -0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <h1>This is my react App</h1>
         <h2> No Level</h2>
         <p>this is a paragraph</p>
+        <p>Another Text</p>
       </header>
     </div>
   );
